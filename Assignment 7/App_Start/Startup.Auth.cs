7dd4394adacc4b726493c4368ef7d7b525af2015
Muhammad Asad Shakeel <m.asadshakeel@gmail.com>
2017-07-08 12:54:02 +0500

    importing project

---
+++ Assignment 7/App_Start/Startup.Auth.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.DataProtection;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using System;
+using Assignment_7.Models;
+
+namespace Assignment_7
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure the db context and user manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            // Configure the sign in cookie
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            app.UseTwitterAuthentication(
+               consumerKey: "BgCTWv397pXhbzd5AZQuXNGjb",
+               consumerSecret: "nbBq2XA1iBISYpYz84ltqEEBzky8PPox68boMWL9lar2WsdIKa");
+
+            app.UseFacebookAuthentication(
+                //appId: "426f62526f636b73",
+                //appSecret: "57686f6120447564652c2049495320526f636b73"
+                appId: "267832260361155",
+                appSecret: "396dc2f812dfc6e8790294341607c9f3"
+            );
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "482253152506-7bsgpvu84890etpapmiioc4om8nqt4a3.apps.googleusercontent.com",
+                ClientSecret = "Ls5yOwALM9EzMWAnR0rJVvId"
+            });
+        }
+    }
+}
